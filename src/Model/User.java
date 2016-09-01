b71dc7350e88370db24e847a8b7858e30a73a548
Marcel Germes <murdock.kaclan@gmail.com>
2016-09-01 13:15:00 +0200

    Users

--- src/Model/User.java
+++ src/Model/User.java
@@ -3,5 +3,31 @@ package Model;
 /**
  * Created by yketd on 1-9-2016.
  */
-public class User {
+public class User
+{
+    private String username;
+    private String password;
+    private boolean isOwner;
+
+    public User(String username, String password, boolean isOwner)
+    {
+        this.username = username;
+        this.password = password;
+        this.isOwner = isOwner;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public boolean isOwner()
+    {
+        return isOwner;
+    }
 }
