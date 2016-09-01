7885f168bcddc4e4b78201a20ee4a891400ac926
Marcel Germes <murdock.kaclan@gmail.com>
2016-09-01 11:52:03 +0200

    Ignorefile and rental form

--- src/SearchRoomServlet.java
+++ src/SearchRoomServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/servlet")
+@WebServlet("/huurder")
 public class SearchRoomServlet extends HttpServlet
 {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
